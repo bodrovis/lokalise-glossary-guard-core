94d97ca150ca74524ba05ce0131728d2583d5625
bodrovis <elskruk@proton.me>
2025-11-03 21:14:41 +0200

    +all [skip ci]

---
+++ pkg/checks/all/all.go
@@ -0,0 +1,19 @@
+package all
+
+import (
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/10_allowed_columns_header"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/11_no_duplicate_header_cells"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/12_no_empty_term_values"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/13_no_duplicate_term_values"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/14_no_orphan_locale_descriptions"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/15_no_invalid_flags"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/1_valid_extension"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/2_valid_encoding"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/3_no_empty_lines"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/4_non_empty_file"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/5_at_least_two_lines"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/6_semicolon_separators"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/7_no_header_spaces"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/8_lowercase_header"
+	_ "github.com/bodrovis/lokalise-glossary-guard-core/pkg/checks/9_term_description_header"
+)
